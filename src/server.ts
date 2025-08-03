0268f0038581c0cecf6f878befead3710dacbc6d
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-08-03 12:37:37 +0600

    setup env

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
 import { Server } from 'http';
 import mongoose from "mongoose";
 import app from './app';
+import { envVars } from './app/config/env';
 
 let server: Server;
 
 const startServer = async () => {
   try {
-    await mongoose.connect("mongodb+srv://ride-booking:UowKd9ub5XLEcksf@cluster0.i1uhr.mongodb.net/Ride-Booking?retryWrites=true&w=majority&appName=Cluster0");
-    console.log("Connected To DataBase");
-
-    server = app.listen(5000, () => {
+    await mongoose.connect(envVars.DB_URL);
+    console.log("Database Connected");
+    
+    server = app.listen(envVars.PORT, () => {
       console.log('Server is Running!!');
     })
 
