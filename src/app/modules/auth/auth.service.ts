0798b175fd42af3db34930637d41c9b7bf14b4a4
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-08-04 21:10:05 +0600

    add jwt complete

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,8 +3,8 @@ import AppError from "../../errorHelpers/AppError";
 import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import bcrypt from "bcryptjs";
-import { generateToken } from "../../utils/jwt";
-import { envVars } from "../../config/env";
+import { createNewAccessTokenWithRefreshToken, createUserToken } from "../../utils/userToken";
+
 const credentialLogin = async (payload: Partial<IUser>) => {
     const { email, password } = payload;
 
@@ -24,13 +24,25 @@ const credentialLogin = async (payload: Partial<IUser>) => {
         role : isUserExist.role 
     }
     
-    const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
+    const userToken = createUserToken(isUserExist);
+    const {password : pass,  ...rest} = isUserExist.toObject();
 
     return {
-        accessToken
+        accessToken : userToken.accessToken,
+        refreshToken : userToken.refreshToken,
+        user : rest
     }
 } 
 
+const getNewAccessToken = async (refreshToken : string ) => {
+   const newAccessToken = await createNewAccessTokenWithRefreshToken(refreshToken);
+
+   return {
+    accessToken : newAccessToken.accessToken
+   }
+}
+
 export const AuthService = {
-    credentialLogin
+    credentialLogin,
+    getNewAccessToken
 }
--- src/app/modules/ride/ride.route.ts
+++ src/app/modules/ride/ride.route.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { RideController } from "./ride.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "../user/user.interface";
 
 const router = Router();
 
-router.post("/request", RideController.createRideRequest);
+router.post("/request", checkAuth(Role.RIDER), RideController.createRideRequest);
 // TODO : Role Base Token Create auth(Role.DIVER, ROLE.ADMIN)
-router.patch("/:id/status", RideController.updateRideStatus);
+router.patch("/:id/status", checkAuth(Role.DIVER) ,RideController.updateRideStatus);
 router.get("/me", RideController.getMyRides);
 
 export const RideRoutes = router;
