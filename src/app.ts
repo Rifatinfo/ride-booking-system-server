b02eda2225561d9c739b4f510fd536d8b5db30d5
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-11-06 13:26:41 +0600

    finishline code add

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,14 @@ import { router } from "./app/routes";
 import { globalErrorHandler } from "./app/middlewares/errorHandler.middleware";
 import { notFound } from "./app/middlewares/notFound";
 import cookieParser from "cookie-parser";
-import { envVars } from "./app/config/env";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(cors({
-    // origin: envVars.FRONTEND_URL,
-    // origin: ['http://localhost:5173'],
-    origin: ['https://ride-booking-clients.vercel.app'],
+    origin: ['http://localhost:5173'],
+    // origin: ['https://ride-booking-clients.vercel.app'],
     credentials: true
 }))
 app.use(cookieParser());
